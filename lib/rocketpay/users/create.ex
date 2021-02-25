bbe89352e7b6b3119bd5855aff5ed15b9836bdcf
Wylliam Bassos <bassoscolorado@gmail.com>
2021-02-25 00:50:54 -0300

    V0.3.1
    --inicio da aula 3

--- lib/rocketpay/users/create.ex
+++ lib/rocketpay/users/create.ex
@@ -1,9 +1,40 @@
 defmodule Rocketpay.User.Create do
-  alias Rocketpay.{Repo, User}
+  alias Ecto.Multi
+  alias Rocketpay.{Account, Repo, User}
 
   def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
+    Multi.new()
+    |> Multi.insert(:create_user, User.changeset(params))
+    |> Multi.run(:create_account, fn repo, %{create_user: user} ->
+      insert_account(user, repo)
+    end)
+    |> Multi.run(:preload_data, fn repo, %{create_user: user} ->
+      preload_data(user, repo)
+    end)
+    |> run_transaction()
+  end
+
+  defp insert_account(user, repo) do
+    user.id
+    |> accout_changeset()
+    |> repo.insert()
+  end
+
+  defp accout_changeset(user_id) do
+    %{user_id: user_id, balance: "0.00"}
+    |> Account.changeset()
+  end
+
+  defp preload_data(user, repo) do
+    {:ok, repo.preload(user, :account)}
+  end
+
+  defp run_transaction(multi) do
+    IO.inspect(Repo.transaction(multi))
+
+    case Repo.transaction(multi) do
+      {:error, _òperations, reason, _changes} -> {:error, reason}
+      {:ok, %{preload_data: user}} -> {:ok, user}
+    end
   end
 end
